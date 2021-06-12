bfdef9fac83fd3b403a6874f3025620a6dd31a80
Rodrigo Cacilhas <batalema@cacilhas.info>
2021-06-12 00:50:47 -0300

    add best time

--- HUD.gd
+++ HUD.gd
@@ -3,9 +3,16 @@ extends Control
 onready var time_label := $Time
 onready var cheese_left_label := $CheeseLeft
 onready var well_done_label := $WellDone
+onready var best_time_label := $BestTime
 var done := false
+var best_time := 10000
 var timer := 0.0
 var cheese_left := 4
+var storage := "user://data.json"
+
+
+func _ready() -> void:
+	read_storage()
 
 
 func _process(delta: float) -> void:
@@ -20,3 +27,25 @@ func score() -> void:
 	if cheese_left <= 0:
 		done = true
 		well_done_label.show()
+		call_deferred("save_score")
+
+
+func read_storage() -> void:
+	var file := File.new()
+	if file.file_exists(storage):
+		file.open(storage, File.READ)
+		var data := JSON.parse(file.get_as_text())
+		if data.result and data.result.best_time:
+			best_time = data.result.best_time
+			best_time_label.text = "Best Time: %04d" % best_time
+			best_time_label.show()
+		file.close()
+
+
+func save_score() -> void:
+	if timer < best_time:
+		var file := File.new()
+		file.open(storage, File.WRITE)
+		file.store_string(JSON.print({best_time = int(timer)}))
+		file.close()
+		read_storage()
